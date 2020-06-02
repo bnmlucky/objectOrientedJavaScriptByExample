fb31d5fe69aa56e5b3e4625b9d903507d99c53c3
Natalia Titova <bnmlucky@gmail.com>
2020-06-02 11:37:24 -0400

    added the properties to the Token class

--- Stage1 - Introducing Four in a Row/Stage1Step5 - Creating Class Files/js/Token.js
+++ Stage1 - Introducing Four in a Row/Stage1Step5 - Creating Class Files/js/Token.js
@@ -1,3 +1,7 @@
 class Token {
-
+    constructor() {
+        this.owner = owner; //reference to the player object that owns the token
+        this.id = `token-${index}-${owner.id}`;
+        this.dropped = false;
+    }
 }
